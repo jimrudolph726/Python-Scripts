fa040f366a8c5c3bfe4811b62e6d327a610146c8
jimrudolph726 <130412104+jimrudolph726@users.noreply.github.com>
2025-02-18 15:01:22 -0600

    Update Lake Parameter Oriented Reformatter FINAL.py

--- Work Scripts/Lake Parameter Oriented Reformatter FINAL.py
+++ Work Scripts/Lake Parameter Oriented Reformatter FINAL.py
@@ -3,36 +3,27 @@ import openpyxl
 import csv
 import pandas as pd
 import numpy as np
-from lake_mappings import *
+from melt_maps import *
 
-current_directory = os.getcwd()
 method_shortname_dict = {2022: method_shortname_rmb_2022,
-                         2023: method_shortname_rmb_2023}
+                         2024: method_shortname_rmb_2023}
 rl_values_dict = {2022: rl_values_rmb_2022,
-                  2023: rl_values_rmb_2023}
+                  2024: rl_values_rmb_2023}
 mdl_values_dict = {2022: mdl_values_rmb_2022,
-                   2023: mdl_values_rmb_2023}
+                   2024: mdl_values_rmb_2023}
 parameter_unit_shortname_dict = {2022: parameter_unit_shortname_rmb_2022,
-                                 2023: parameter_unit_shortname_rmb_2023}
+                                 2024: parameter_unit_shortname_rmb_2023}
 parameter_unit_symbol_dict = {2022: parameter_unit_symbol_rmb_2022,
-                              2023: parameter_unit_symbol_rmb_2023}
+                              2024: parameter_unit_symbol_rmb_2023}
 parameter_type_dict = {2022: parameter_type_rmb_2022,
-                       2023: parameter_type_rmb_2023}
+                       2024: parameter_type_rmb_2023}
 
-def combine_tabs(wb):
-    combined_data = []
-    seen_header = False
-    for sheet_name in wb.sheetnames:
-        if sheet_name not in ["Metadata", "ALL", "Como", "ALLDATA", "CRWD"]:
-            ws = wb[sheet_name]
-            for row in ws.iter_rows(values_only=True):
-                if not seen_header:
-                    combined_data.append(row)
-                    if "LAKENAME" in row:
-                        seen_header = True
-                elif row[0] in ["COMO", "CROSBY", "Little Crosby", "LOEB", "MCCARRON"]:
-                    combined_data.append(row)
-    return combined_data
+def process_xlsx_files(input_folder):
+    for file_name in os.listdir(input_folder):
+        if file_name.endswith(".xlsx"):
+            input_file_path = os.path.join(input_folder, file_name)
+            output_file_path = os.path.join(input_folder, os.path.splitext(file_name)[0] + ".csv")
+            process_workbook(input_file_path, output_file_path)
 
 def process_workbook(input_file_path, output_file_path):
     wb = openpyxl.load_workbook(input_file_path)
@@ -42,31 +33,42 @@ def process_workbook(input_file_path, output_file_path):
         for row in combined_data:
             writer.writerow(row)
 
-def process_xlsx_files(input_folder):
-    for file_name in os.listdir(input_folder):
-        if file_name.endswith(".xlsx"):
-            input_file_path = os.path.join(input_folder, file_name)
-            output_file_path = os.path.join(input_folder, os.path.splitext(file_name)[0] + ".csv")
-
-            process_workbook(input_file_path, output_file_path)
+def combine_tabs(wb):
+    combined_data = []
+    original_headers = ['LAKENAME', 'DNRID', 'Date', 'SITE', 'Depth-m', 'STRAT-M', 'EPIL-Y,N', 
+                        'DO-mg/L', 'T-°C', 'SEC-m', 'Chl-a', 'TP (MG/L)', 'Ortho-P (mg/L)', 'PH-SU', 
+                        'TURB-NTU', 'CL-MG/L', 'SPCON-UMHOS', 'NO3N-MG/L', 'NH3N-MG/L', 
+                        'TKN-MG/L', 'TPM-MG/L', 'OPM-MG/L']
+    seen_header = False
+    
+    for sheet_name in wb.sheetnames:
+        # Skip certain sheet names
+        if sheet_name not in ["Metadata", "ALL", "Como", "ALLDATA", "CRWD"]:
+            ws = wb[sheet_name]
+            for row in ws.iter_rows(values_only=True):
+                # If header hasn't been added, add it explicitly
+                if not seen_header:
+                    combined_data.append(original_headers)  # Use the specified headers
+                    seen_header = True
+                # Add rows that match specified lake names
+                if row[0] in ["COMO", "CROSBY", "Little Crosby", "LOEB", "MCCARRON"]:
+                    combined_data.append(row)
+    return combined_data
 
 def process_csv(input_folder, output_folder):
     try:
         filecount = 0
-        current_directory = os.getcwd()
-        # Initialize an empty list to store melted DataFrames
-        
-
         for filename in os.listdir(input_folder):
-            melted_dfs = []
+            
+        
             if filename.endswith(".csv"):
-                
-                # Read CSV file using pandas
+                # Read CSV file and create pandas dataframe
                 input_filepath = os.path.join(input_folder, filename)
                 df = pd.read_csv(input_filepath, encoding='latin-1')
                 year = int(df['Date'][0][:4])
-
-                post_rows = process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year)
+                
+                # Apply melt function to convert wide data to long data
+                post_rows = melt_dataframe(df, filename, output_folder, year)
 
                 filecount += 1
                 print(f"Successfully processed {filename}")
@@ -75,11 +77,12 @@ def process_csv(input_folder, output_folder):
                 os.remove(input_filepath)
         print(f"All processed data saved to: {os.path.abspath(output_folder)}")
         print(f"Total files processed: {filecount}")
-    
+
     except Exception as e:
         print(f"Error occurred during processing: {e}")
 
-def process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year):
+def melt_dataframe(df, filename, output_folder, year):
+    melted_dfs = []
     #Modify the columns and process the data for years greater than 2021
     try:
         def rename_depth_columns(df):
@@ -114,7 +117,7 @@ def process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year
             df.at[index, 'Sample number'] = sample_number
 
         for param in param_columns:
-            melted_df = pd.melt(df, id_vars=other_columns, value_vars=[param], var_name="Parameter", value_name="Value")
+            melted_df = pd.melt(df, id_vars=other_columns, value_vars=[param], var_name="Parameter type", value_name="Parameter value")
             melted_dfs.append(melted_df)
         
         # Concatenate all melted DataFrames into a single DataFrame
@@ -123,7 +126,7 @@ def process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year
         combined_df['Date'] = pd.to_datetime(combined_df['Date'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
         
         combined_df['Measuring program shortname'] = 'Lake'
-        combined_df.rename(columns={'SITE':'Sampling location', 'Date':'DateTime', 'Parameter': 'Parameter type', 'Value': 'Parameter value'}, inplace=True)
+        combined_df.rename(columns={'SITE':'Sampling location', 'Date':'DateTime'}, inplace=True)
         
         # format DateTime column
         combined_df['DateTime'] = pd.to_datetime(combined_df['DateTime'])
@@ -131,7 +134,7 @@ def process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year
         split_date = combined_df['DateTime'].str.split('/')
         reformatted_date = split_date.str[2] + split_date.str[0].str.zfill(2) + split_date.str[1].str.zfill(2)
         
-        # Add the reformatted date to the DataFrame
+        # Add and format all necessary columns required for import
         combined_df['Reformatted Date'] = reformatted_date
         combined_df['Reformatted LAKENAME'] = combined_df['LAKENAME'].map(reformatted_lakename_rmb)
         combined_df['Sampling number'] = combined_df['Reformatted Date'].astype(str) + combined_df['Reformatted LAKENAME'].astype(str) + combined_df['Sampling location'].astype(str)
@@ -154,26 +157,27 @@ def process_year_greater_than_2021(df, filename, melted_dfs, output_folder, year
         combined_df.drop(columns=['LAKENAME'], inplace=True)
         combined_df = combined_df[column_order]
         combined_df.dropna(subset=['Parameter value'], inplace=True)
-        # combined_df_no_sample_numbers = combined_df.copy()
-        # combined_df_no_sample_numbers['Sample number'] = ''
+        combined_df_no_sample_numbers = combined_df.copy()
+        combined_df_no_sample_numbers['Sample number'] = ''
+        
         # Save formatted csv
-
         filename = os.path.splitext(filename)[0]
+        
         # Output with sample numbers
         output_name =  f'{output_folder}{filename}_processed.csv'
         combined_df.to_csv(output_name, index=False)
 
         # Output without sample numbers
-        # output_name =  f'{output_folder}{filename}_no_sample_numbers_processed.csv'
-        # combined_df_no_sample_numbers.to_csv(output_name, index=False)
+        output_name =  f'{output_folder}{filename}_no_sample_numbers_processed.csv'
+        combined_df_no_sample_numbers.to_csv(output_name, index=False)
+        
     except Exception as e:
         print(f"Error occurred during processing for year {year}: {e}")
     return combined_df.shape[0]
-# Provide input folder path and output file path
+
 input_folder = "input/"
 output_folder = "output/"
 
 # Call the function to process CSV files and save the combined data
-
 process_xlsx_files(input_folder)
 process_csv(input_folder, output_folder)
